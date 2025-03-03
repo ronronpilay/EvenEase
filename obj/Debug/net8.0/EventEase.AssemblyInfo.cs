d9b600f3abc50169c961142d517febd8ba8afaef
Adam <bryanadams.son1988@gmail.com>
2025-03-03 13:17:54 +0800

    Initial commit of my EventEase project

--- obj/Debug/net8.0/EventEase.AssemblyInfo.cs
+++ obj/Debug/net8.0/EventEase.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5630596ed8311d1a6fae7f20a3b0374cdf1e353")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
